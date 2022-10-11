66d3335c59f863e83beedceca88ac3c0f49a2123
jgrant928 <jsgrant928@gmail.com>
2022-10-11 19:06:41 -0400

    expanded out javascript

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -16,6 +16,42 @@ var feedbackEl = document.getElementById('feedback');
 var sfxRight = new Audio('assets/sfx/correct.wav');
 var sfxWrong = new Audio('assets/sfx/incorrect.wav');
 
+var questions = [
+    {
+      title: 'Commonly used data types DO NOT include:',
+      choices: ['strings', 'booleans', 'alerts', 'numbers'],
+      answer: 'alerts',
+    },
+    {
+      title: 'The condition in an if / else statement is enclosed within ____.',
+      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+      answer: 'parentheses',
+    },
+    {
+      title: 'Arrays in JavaScript can be used to store ____.',
+      choices: [
+        'numbers and strings',
+        'other arrays',
+        'booleans',
+        'all of the above',
+      ],
+      answer: 'all of the above',
+    },
+    {
+      title:
+        'String values must be enclosed within ____ when being assigned to variables.',
+      choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+      answer: 'quotes',
+    },
+    {
+      title:
+        'A very useful tool used during development and debugging for printing content to the debugger is:',
+      choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+      answer: 'console.log',
+    },
+  ];
+
+
 
 function startQuiz() {
     // hide start screen after pressing button
@@ -27,7 +63,7 @@ function startQuiz() {
 
 
     //start timer (high)
-    timerId = setInterval(clockTick, 60,000);
+    timerId = setInterval(clockTick, 600,000);
 //you need to declare a var named timerId. You will also need to use setInterval and clockTick
 
     //show starting time (high)
@@ -50,19 +86,18 @@ function getQuestion() { //this function is going to get the data from the quest
     // create a for loop that creates the choice elements
     for (var i = 0; i < currentQuestion.choices.length; i++) {
         // create new button for each choice
-        //.createElement
-        var choicesEl = document.createElement('button');
+        var buttonEl = document.createElement('button');
+        buttonEl.setAttribute('class', 'choice');
+        buttonEl.setAttribute('value', currentQuestion.choices[i]);
         // attach click event listener to each choice
-        choicesEl.addEventListener('click', questionClick);
+        buttonEl.addEventListener('click', questionClick);
         //.setAttribute (set a class="choice")
-        choicesEl.setAttribute('class', 'choice');
         // display on the page
+       buttonEl.textContent = currentQuestion.choices[i];
 
-        choicesEl.textContent = currentQuestion.choices[i];
+        // display on the page
+        choicesEl.appendChild(buttonEl);
         //.textContent
-
-        //.appendChild
-        choicesEl.appendChild(choicesEl);
 
     }
 }
@@ -75,28 +110,37 @@ function questionClick(event) {
         return;
     }
 
-    // check if user guessed right or wrong
-    if (true) { //replace true with a conditional statement that checks if the clicked choice button's value is the same as the questions[currentQuestionIndex]'s answer
+    // create if else function to check if user guessed right or wrong
+    //conditional statement that checks if the clicked choice button's value is not the same as the questions[currentQuestionIndex]'s answer
+    if (!buttonEl.value === questions[currentQuestionIndex].answer) { 
         //incorrect answer scenario
-
-
-        // penalize time
-        // display new time on page
-    } else {
-        //correct scenario
-
+        // play "wrong" sound effect
+        time -= 15;
+        sfxWrong.play();
+        // wrong feedback on page for half a second
+        feedbackEl.textContent = 'Wrong!';
+        feedbackEl.setAttribute('class', 'feedback');
+        setTimeout(function() {
+            feedbackEl.setAttribute('class', 'feedback hide');
+        }, 1000);
+        timerEl.textContent = time;
+    } else { (buttonEl.value === questions[currentQuestionIndex].answer)  
+        sfxRight.play();
+        feedbackEl.textContent = 'Right!';
+        feedbackEl.setAttribute('class', 'feedback');
+        setTimeout(function() {
+            feedbackEl.setAttribute('class', 'feedback hide');
+        }, 1000);
+        currentQuestionIndex++;
+    }   
         // move to next question
-    }
-    // flash right/wrong feedback on page
-
-    // move to next question
-    currentQuestionIndex++;
-
+    
     // check if we've run out of questions
     if (time <= 0 || currentQuestionIndex === questions.length) {
         quizEnd();
     } else {
         getQuestion();
+
     }
 }
 
@@ -137,13 +181,18 @@ function saveHighscore() {
 
         //JSON.parse
         // get saved scores from localstorage (highscores), or if not any, set to empty array
-        
+        var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
 
         // format new score object for current user
+        var newScore = {
+            score: time,
+            initials: initials
+        };
         
 
         // save to localstorage
-        
+        highscores.push(newScore);
+        window.localStorage.setItem('highscores', JSON.stringify(highscores));
 
         // redirect to next page
         window.location.href = 'highscores.html';
